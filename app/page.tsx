de96e49ea21c5fe150785bfc024edcbbaf8c0282
franckpedron <franckpedrondev@gmail.com>
2025-02-22 05:31:21 +0100

    Creation du model et de la base de données

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,34 @@ import Header from "@components/Header";
 import AddTask from "@components/AddTask";
 
 export default function Home() {
-  const [task, setTask] = useState("");
+    const [task, setTask] = useState("");
+    const [loading, setIsLoading] = useState(true);
 
-  const handleCreateTask = async() => {};
+    const handleCreateTask = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch("/api/task/new", {
+                method: 'POST',
+                body: JSON.stringify({
+                    task: task
+                })
+            })
+            if (response.ok) {
+                setTask("")
+            }
+            else {
+                console.log("Error")
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        setIsLoading(false);
+    };
 
     return (
-      <>
-        <Header />
-          <AddTask task={task} setTask={setTask} handleCreateTask={handleCreateTask} />
-      </>
-  )
+        <>
+            <Header/>
+            <AddTask task={task} setTask={setTask} handleCreateTask={handleCreateTask}/>
+        </>
+    )
 }
